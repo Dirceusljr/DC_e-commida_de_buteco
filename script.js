93b8ec94bee3a2a28420f22cbf7bae41cd1e018f
DIRCEU DOS SANTOS LIMA JUNIOR <DIRCEU0154@DIGITALCOLLEGE.LOCAL>
2023-11-17 20:50:04 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let numeroDigitado = document.getElementById('numero')
 const btnTabuada = document.getElementById('criar')
 const tabuada = document.getElementById('tabuada')
+const btnLimpar = document.getElementById('limpar')
 
 function MostrarTabuada (e) {
     e.preventDefault();
@@ -12,6 +13,13 @@ function MostrarTabuada (e) {
                 ${numeroDigitado.value} x ${i} = ${Number(numeroDigitado.value * i)}            `
             tabuada.appendChild(lista)
         i++
-}}
+}
+numeroDigitado.value =''
+}
+
+function Limpartela() {
+    tabuada.innerHTML = ''
+}
 
 btnTabuada.addEventListener('click', (btnTabuada) => MostrarTabuada (btnTabuada))
+btnLimpar.addEventListener('click', Limpartela)
